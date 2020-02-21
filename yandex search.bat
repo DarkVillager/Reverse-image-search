b8a6fbb1beef30bae3f6cc137cdbe4c453be8975
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2020-02-21 17:23:08 +0100

    Update yandex search.bat

--- yandex search.bat
+++ yandex search.bat
@@ -3,6 +3,7 @@ set url="https://yandex.com/images/search?source=collections&rpt=imageview&url="
 Paste
 for /f %%i in ('paste') do set PASTED=%%i
 set urls=%url%%pasted%
-@echo %urls%
 start chrome %urls%
+@echo ""
+@echo Searching yandex for %pasted%
 pause
