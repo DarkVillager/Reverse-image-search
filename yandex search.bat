b6ab1e6e26cc7a5577e03eba246c423adeb83487
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2020-02-21 17:09:00 +0100

    Yandex Image Search CMD
    Reqs: https://www.c3scripts.com/tutorials/msdos/paste.html#exe

--- yandex search.bat
+++ yandex search.bat
@@ -1,3 +1,4 @@
+@echo Reqs: https://www.c3scripts.com/tutorials/msdos/paste.html#exe
 set url="https://yandex.com/images/search?source=collections&rpt=imageview&url="
 Paste
 for /f %%i in ('paste') do set PASTED=%%i
